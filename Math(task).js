60dfe0b9ceab3bf6d48d355680114f9fd88d87fc
mernaawd <125801027+mernaawd@users.noreply.github.com>
2024-05-26 00:50:38 +0300

    Add files via upload

---
+++ Math(task).js
@@ -0,0 +1,18 @@
+// Task1
+var r= prompt("Please enter the raduis");
+var raduis= parseInt(r);
+console.log(typeof(raduis));
+console.log(raduis);
+var area= raduis*raduis* Math.PI ; 
+console.log(area);
+alert("Your Area is " +area+"");
+
+
+// Task2
+var S= prompt("Please enter another value");
+var value= parseInt(S);
+console.log(typeof(value));
+console.log(value);
+var total= Math. sqrt(value);
+console.log(total);
+alert("Your Sqr is " +total+"");
